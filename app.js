b36ff6d0f565e8c8008b0169289b5383bff4c2a8
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-05-18 17:25:11 -0300

    Primer Ejercicio

---
+++ app.js
@@ -0,0 +1,9 @@
+
+/*Ejercicio 1: Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de 
+ambos números en una 3er variable.
+*/
+ var numUno = 10;
+ var numDos = 20;
+var numResultadoSuma = numUno + numDos ;
+
+/*Ejercicio 2 */

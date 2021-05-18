717f0c62778814789f42c34d7377f06dbaba4e5a
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-05-18 17:38:03 -0300

    Segundo Ejercicio

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 /*Ejercicio 1: Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de 
-ambos números en una 3er variable.
-*/
+ambos números en una 3er variable.*/
  var numUno = 10;
  var numDos = 20;
-var numResultadoSuma = numUno + numDos ;
+ var numResultadoSuma = numUno + numDos ;
 
-/*Ejercicio 2 */
+/*Ejercicio 2: Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+var primeraPalabra ='palabraUno';
+var segundaPalabra = 'palabraDos';
+var tercerPalabra = primeraPalabra + segundaPalabra;

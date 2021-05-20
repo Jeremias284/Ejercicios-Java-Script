52d7c55a96b2babb6e6f699c423075296bcc0f9b
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-05-19 21:23:55 -0300

    Ejercicio 18

--- app.js
+++ app.js
@@ -118,6 +118,44 @@ console.log("Greater than 0,5");
 }
 console.log(numero);
 
+/*Ejercicio 18:  Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes 
+mensajes de alerta:
+i) “Bebe” si la edad es menor a 2 años
+ii) “Nino” si la edad es entre 2 y 12 años
+iii) “Adolecente” si la edad es entre 13 y 19 años
+iv) “Joven” si la edad está entre 20 y 30 años
+v) “Adulto” entre 31 y 60 años
+vi) “Adulto mayor” entre 61 y 75 años
+vii) “Anciano” si es mayor a 75 años */
+var Age = Math.floor(Math.random()* 101);
+if(Age <2)
+{
+    console.log("Bebe");
+}
+if(Age > 2 && Age <12)
+{
+    console.log("Niño");
+}
+if(Age > 13 && Age <19)
+{
+    console.log("Adolecente");
+}
+if(Age > 20 && Age  < 30)
+{
+    console.log("Joven");
+}
+if(Age > 31 && Age  < 60)
+{
+    console.log("Adulto");
+}
+if(Age > 61 && Age  < 75)
+{
+    console.log("Adulto Mayor");
+}
+else{
+    console.log("Anciano")
+}
+console.log(Age);
 
 
 
